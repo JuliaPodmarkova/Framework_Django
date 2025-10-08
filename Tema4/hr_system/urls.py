9021b26a095766e88e5371847befcc6f481d597c
Julia <151451268+JuliaPodmarkova@users.noreply.github.com>
2025-10-08 17:19:56 +0300

    Add files via upload

---
+++ Tema4/hr_system/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from employees.views import HomePageView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('', HomePageView.as_view(), name='home'),
+
+    path('employees/', include('employees.urls', namespace='employees')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
