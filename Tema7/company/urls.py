7d405663fbf5bc2b714dc4df7b9eb8063992f657
Julia <151451268+JuliaPodmarkova@users.noreply.github.com>
2025-11-01 19:10:14 +0300

    Delete Tema7 directory

--- Tema7/company/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PositionCreateView
-
-urlpatterns = [
-
-    path('positions/add/', PositionCreateView.as_view(), name="position_create"),
-
-]
--- Tema7/company/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import CreateView
-from .models import Position
-
-class PositionCreateView(CreateView):
-    model = Position
-    fields = ['name', 'slug']
-    template_name = 'company/position_form.html'
-    success_url = '/'
